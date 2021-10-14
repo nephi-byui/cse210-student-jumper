07aba859f986aaf88d51d68178b20ef3e98d63da
nephi-byui <byui@nephi.malit.me>
2021-10-14 11:28:39 +0800

    started director class

---
+++ jumper/game/director.py
@@ -0,0 +1,17 @@
+class Director():
+    """
+    
+    ATTRIBUTES:
+        word (Word)   :   an instance of Word
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def start_game(self):
+        print('game')
+
+    def word(self):
+        self.word = Word()
+
+    
