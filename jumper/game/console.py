14da8dd20fb80d9e39f7446373b34a3ae26c60e6
nephi-byui <byui@nephi.malit.me>
2021-10-15 00:38:57 +0800

    fleshing things out

--- jumper/game/console.py
+++ jumper/game/console.py
@@ -1,23 +1,44 @@
-import random
-
 class Console:
-    """A code template for a computer console. The responsibility of this 
-    class of objects is to get text or numerical input and display text output.
-    
-    Stereotype:
-        Service Provider, Interfacer
+    """The responsibility of this class of objects is to:
+        * get text or numerical input
+        * display text output.
+    This class will make it easier to port input or output if the program is
+    to be ported to a GUI or Web App, for example.
     Attributes:
-        prompt (string): The prompt to display on each line.
+        none
     """
-     
-    def read(self, prompt):
+    def take_input(self, prompt):
         """Gets text input from the user through the screen.
         Args: 
-            self (Screen): An instance of Screen.
+            self (Console): An instance of Console
             prompt (string): The prompt to display to the user.
         Returns:
             string: The user's input as text.
         """
         return input(prompt)
 
- 
+    def display_output(self, string):
+        """ Prints text to be read by the user
+            Instead of using print() in your functions
+            use this method - console.display_output()
+        Args:
+            self (Console): An instance of Console
+            string: the string to be printed
+        """
+        print(string)
+
+# for testing
+def main():
+    # Create a Console object
+
+    console = Console()
+
+    # Ask a question
+    favorite_food = console.take_input("What is your favorite food? ")
+
+    # Display output
+    display_string = f"Your favorite food is {favorite_food}!"
+    console.display_output(display_string)
+
+if __name__ == "__main__":
+    main()
--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,17 +1,68 @@
+
+from game.console import Console
+from game.wordlist_generator import WordListGenerator
+from game.word import Word
+#from game.parachute import Parachute
+
 class Director():
     """
     
     ATTRIBUTES:
-        word (Word)   :   an instance of Word
+        word (Word)             :   an instance of Word
+        console (Console)       :   an instance of Console
+        parachute (Parachute)   :   an instance of Parachute
+
+        keep_playing (BOOL)     :   game will end
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.keep_playing = True
+
+        self.console = Console()
+        self.wordlist_generator = WordListGenerator()
+        
+
+        #self.parachute = Parachute()
+        #self.word = Word()
+        self.word_list = []
+
+
+    def configure_list(self):
+        """Asks the user if they want to pick an external word list or use the default
+        ARGS:
+            self (Director): an instance of Director
+        RETURNS:
+            a list of words
+        """
+        while True:
+            user_input = self.console.take_input("Would you like to use an external word list? (y/n) ")
+
+            if user_input in ["Y", "y", "N", "n"]:
+                break
+            else:
+                ("Invalid input.")
+                continue
+        
+        # use external
+        if user_input in ["Y", "y"]:
+            file_path = self.wordlist_generator.GUI_select_file("Select word list")
+            list = self.wordlist_generator.import_list(file_path)
+
+        # use default
+        else:
+            list = self.wordlist_generator.default_list()
+
+        return list
+
 
     def start_game(self):
-        print('game')
+
+        # pick a word list
+        self.configure_list()
 
     def word(self):
         self.word = Word()
+
+
 
     
